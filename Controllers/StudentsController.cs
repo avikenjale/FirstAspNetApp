d1a327abea4dde344b390e13777f109b5a21ec93
avikenjale <avikenjale@gmail.com>
2016-04-21 08:24:42 -0700

    Initial Commit

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+
+namespace FirstAspNetApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class StudentsController:Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[]{"Student 1","student 2"};
+        }
+    }
+}
